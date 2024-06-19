9201f72e064614a1457d58a40f21c7a5f61238f5
Thinh Tran <duythinht@gmail.com>
2024-06-19 16:27:24 +0700

    Update new morning message

--- jobs/slack-notify-code-morning.sh
+++ jobs/slack-notify-code-morning.sh
@@ -6,4 +6,4 @@ source 'lib/slack.sh'
 
 export LC_ALL=en_US.UTF-8
 
-slack.send_message $SLACK_CHANNEL "Chào bủi sáng, cốt chưa anh em? Chưa code thì qua <#C0UQ8TKLJ|music> nghe nhạc nhé! https://radio.0x97a.com/"
+slack.send_message $SLACK_CHANNEL "Chào bủi sáng, sẵn sàng nhảy việc chưa anh em? Rồi thì chơi ít <#C0UQ8TKLJ|music> trong lúc giải vài bài <#C6XNQB6LB|algorithm> nhé! https://radio.0x97a.com/"
